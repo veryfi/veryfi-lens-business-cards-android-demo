d2ec12ecd0ef43ff1e2f611f0b82b2c8d1e6f8ab
Yuber Garcia <yubergarcia@veryfi.com>
2022-06-09 22:10:57 -0500

    build.gradle updated with new Lens version

--- LensBusinessCardsDemo/app/src/main/java/com/veryfi/lens/business/cards/demo/Application.kt
+++ LensBusinessCardsDemo/app/src/main/java/com/veryfi/lens/business/cards/demo/Application.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.veryfi.lens.VeryfiLens
 import com.veryfi.lens.VeryfiLensCredentials
 import com.veryfi.lens.VeryfiLensSettings
+import com.veryfi.lens.helpers.DocumentType
 
 class Application: Application() {
 
@@ -26,7 +27,7 @@ class Application: Application() {
 
         //optional settings
         val veryfiLensSettings = VeryfiLensSettings()
-        veryfiLensSettings.documentTypes = arrayListOf("business_card")
+        veryfiLensSettings.documentTypes = arrayListOf(DocumentType.BUSINESS_CARD)
         veryfiLensSettings.showDocumentTypes = true
 
         //configure lens
