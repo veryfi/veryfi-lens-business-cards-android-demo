692c8f55898dc1223d9bdcafb6d7155c3c6dd972
Bernardo Restrepo <bernardorestrepo@veryfi.com>
2024-05-17 09:36:59 -0500

    LP-937: Updated Android Lens

--- app/src/main/java/com/veryfi/lens/business/cards/demo/logs/LogsActivity.kt
+++ app/src/main/java/com/veryfi/lens/business/cards/demo/logs/LogsActivity.kt
@@ -20,8 +20,8 @@ import org.json.JSONObject
 class LogsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLogsBinding
-    private lateinit var adapter: LogsAdapter
     private var logsEventData: ArrayList<Log> = ArrayList()
+    private lateinit var adapter: LogsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,9 +29,9 @@ class LogsActivity : AppCompatActivity() {
         setContentView(binding.root)
         applicationContext?.let { ThemeHelper.setSecondaryColorToStatusBar(this, it) }
         setUpVeryfiLensDelegate()
-        VeryfiLens.showCamera()
         setUpToolBar()
         loadData()
+        VeryfiLens.showCamera()
     }
 
     private fun setUpToolBar() {
@@ -71,36 +71,37 @@ class LogsActivity : AppCompatActivity() {
         val log = Log()
         log.message = json
         val status = if (json.has(STATUS)) json.getString(STATUS) else ""
-
         when (status) {
+            CLOSE -> log.title = resources.getString(R.string.logs_result)
             START -> log.title = resources.getString(R.string.logs_start_uploading)
             IN_PROGRESS -> {
                 when (json.getString(MSG)) {
                     IMG_THUMBNAIL -> log.title = resources.getString(R.string.logs_thumbnail)
                     IMG_ORIGINAL -> log.title =
                         resources.getString(R.string.logs_original_image)
+
                     PROGRESS -> log.title = resources.getString(R.string.logs_extraction)
                 }
             }
+
             DONE -> log.title = resources.getString(R.string.logs_result)
             REMOVED -> {
                 log.title = resources.getString(R.string.logs_remove_cache_data)
             }
+
             EXCEPTION -> log.title = resources.getString(R.string.logs_exception)
             ERROR -> log.title = resources.getString(R.string.logs_error)
             FAILED -> log.title = resources.getString(R.string.logs_failed)
             else -> log.title = "Other"
         }
 
-        if (!status.equals(CLOSE)) {
-            logsEventData.add(log)
-            adapter.addItem(log)
-            adapter.notifyDataSetChanged()
-            binding.timelineRv.scrollToPosition(adapter.itemCount - 1)
-        } else {
-            if (logsEventData.size == 0) {
-                finish()
-            }
+        logsEventData.add(log)
+        adapter.addItem(log)
+        adapter.notifyDataSetChanged()
+        binding.timelineRv.scrollToPosition(adapter.itemCount - 1)
+
+        if (status.equals(CLOSE) && logsEventData.size == 0) {
+            finish()
         }
     }
 
